25c2c04abd43f3d1f54dcdb1dbec9642293ed885
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-11-06 16:09:20 -0500

    Added describe block to simple crop boxes tests

--- faceIsolation.test.js
+++ faceIsolation.test.js
@@ -4,20 +4,22 @@ var easyimg = require('easyimage');
 
 var outputDir = './output';
 
-test('simple crop box in center', () => {
-	expect(faceIsolation.getFaceCropBox(100, 100, 45, 45, 10, 10, 2)).toEqual({width:20, height:20, x: 40, y: 40});
-});
-test('simple crop box in center right', () => {
-	expect(faceIsolation.getFaceCropBox(100, 100, 90, 45, 10, 10, 2)).toEqual({width:15, height:20, x: 85, y: 40});
-});
-test('simple crop box in center left', () => {
-	expect(faceIsolation.getFaceCropBox(100, 100, 0, 45, 10, 10, 2)).toEqual({width:15, height:20, x: 0, y: 40});
-});
-test('simple crop box in top center', () => {
-	expect(faceIsolation.getFaceCropBox(100, 100, 45, 90, 10, 10, 2)).toEqual({width:20, height:15, x: 40, y: 85});
-});
-test('simple crop box in bottom center', () => {
-	expect(faceIsolation.getFaceCropBox(100, 100, 45, 0, 10, 10, 2)).toEqual({width:20, height:15, x: 40, y: 0});
+describe('simple crop boxes', () => {
+	test('simple crop box in center', () => {
+		expect(faceIsolation.getFaceCropBox(100, 100, 45, 45, 10, 10, 2)).toEqual({width:20, height:20, x: 40, y: 40});
+	});
+	test('simple crop box in center right', () => {
+		expect(faceIsolation.getFaceCropBox(100, 100, 90, 45, 10, 10, 2)).toEqual({width:15, height:20, x: 85, y: 40});
+	});
+	test('simple crop box in center left', () => {
+		expect(faceIsolation.getFaceCropBox(100, 100, 0, 45, 10, 10, 2)).toEqual({width:15, height:20, x: 0, y: 40});
+	});
+	test('simple crop box in top center', () => {
+		expect(faceIsolation.getFaceCropBox(100, 100, 45, 90, 10, 10, 2)).toEqual({width:20, height:15, x: 40, y: 85});
+	});
+	test('simple crop box in bottom center', () => {
+		expect(faceIsolation.getFaceCropBox(100, 100, 45, 0, 10, 10, 2)).toEqual({width:20, height:15, x: 40, y: 0});
+	});
 });
 
 describe('get face crop arguments', () => {
