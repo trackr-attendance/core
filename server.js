09b39281d83bac6c5ab12bde991aefebbcffd57f
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-11-09 15:20:35 -0500

    Added Facial Isolation from Azure

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var upload = require('./upload');
+var faces = require('./faces');
 
-upload.uploadFile('test001.jpg').then(function (data){
-	console.log("resolved", data);
+upload.uploadFile('test001.jpg').then(function (photo){
+	faces.find(photo.Location).then(function (faces){
+		console.log(faces);
+	});
 });
