29f97525aa5ca0d7a3d4342900bbd1335625c9ac
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-11-28 15:20:12 -0500

    Refactored Attendance into Snapshot

---
+++ attendance.js
@@ -0,0 +1,22 @@
+var upload = require('./upload');
+var faces = require('./faces');
+var faceIsolation = require('./faceIsolation');
+var identify = require('./identification');
+var Q = require('q');
+
+exports.snapshot = function(file, collection){
+	return upload.uploadFile(file).then(function (photo){
+		return faces.find(photo.Location).then(function (faces){
+			return faceIsolation.generateIndividualFacePhotos(file, faces).then(function(photos){
+				return Q.all(photos.map(function (face) {
+					return identify.attendance(collection, face.path);
+				})).then(function (data){
+					return { attendance: data };
+				});
+			});
+		}).then(function (data){
+			data.frame = photo
+			return data;
+		});
+	});
+}
