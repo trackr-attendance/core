adc742bb6cc8715b37e21264feed1a6668766dd8
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2017-12-03 02:30:53 -0500

    Added Engagement Update

--- parser.js
+++ parser.js
@@ -5,7 +5,7 @@ var simpleAttendance = output.attendance.map(function (snapshot) {
 		if (!("id" in student) || (typeof student.id == 'undefined')){
 			return -1;
 		}else {
-			return student.id
+			return Number(student.id);
 		}
 	});
 });
@@ -13,26 +13,24 @@ var simpleAttendance = output.attendance.map(function (snapshot) {
 // Quick Recognition
 var recognised = new Set([].concat.apply([], simpleAttendance));
 recognised.delete(-1);
-console.log(recognised);
+recognised = Array.from(recognised).sort();
 
-// Frequency Map:
-attendance = {};
-output.roster.students.forEach(function (student){
-	attendance[student.id] = {
-		first: student.first,
-		last: student.last,
-		attendance: Array.from(new Array(simpleAttendance.length), (x,i) => 0)
-	};
+//compile avg engagements from each frame
+var engagement =  output.attendance.map(function(record, index){
+	return {
+		time: index + 1,
+		score: record.attendance.reduce(function (accumulator, record, index){
+			if (("engagement" in record) || (typeof record.engagement !== 'undefined')){
+				var score = ((index)*accumulator + record.engagement)/(index+1);
+				return score;
+			}else{
+				if (!accumulator){
+					return 0;
+				}
+				return accumulator;
+			}
+		}, 0),
+	}
 });
 
-simpleAttendance.forEach(function (snapshot, index){
-	snapshot.forEach(function (student){
-		if (student > 0){
-			attendance[student]['attendance'][index] = 1;
-		}
-	});
-});
-
-Object.keys(attendance).forEach(function(key) {
-    console.log(attendance[key]['first'] + ' ' + attendance[key]['last'],',', attendance[key]['attendance'].join(', '));
-});
+console.log(recognised, engagement);
